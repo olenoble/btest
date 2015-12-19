ba19d6dfcdf55bda3756420a107035516d8525f4
olenoble <olenoble@users.noreply.github.com>
2015-12-19 10:33:45 +0000

    Create datetools.py

---
+++ datetools.py
@@ -0,0 +1,13 @@
+from datetime import datetime, date, time, timedelta, tzinfo
+from dateutil import parser
+import numpy as np
+
+
+def dateparse(x, american_convention=None, asdate=True):
+    out = parser.parse(x, dayfirst=american_convention)
+    return out.date() if asdate else out
+
+
+def daystep(x, scaling=365, excludefirst=False, firstvalue=0.0):
+    dt = [x.days / (1.0 * scaling) for x in np.diff(x)]
+    return dt if excludefirst else [firstvalue] + dt
