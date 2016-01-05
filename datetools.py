eead35464c0a445d92a17baa920de95dcf5b1bbc
olenoble <olenoble@users.noreply.github.com>
2016-01-05 21:29:20 +0000

    Update datetools.py

--- datetools.py
+++ datetools.py
@@ -2,6 +2,7 @@ from datetime import datetime, date, time, timedelta, tzinfo
 from dateutil import parser
 import numpy as np
 
+## implement time series (from, to, step,... monthly, xth day of month, etc...)
 
 def dateparse(x, american_convention=None, asdate=True):
     out = parser.parse(x, dayfirst=american_convention)
